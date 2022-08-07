83b32ec873c05d57ccedb10731a182b095e6debf
szqyqxgs <2590708913@qq.com>
2022-08-08 00:03:45 +0800

    新增【化妆美妆】图标

---
+++ icon-park/src/makeups/components.jsx
@@ -0,0 +1,322 @@
+import React from 'react'
+import {
+    AfroPick,
+    BarberBrush,
+    BarberClippers,
+    BeautyInstrument,
+    Blade,
+    Comb,
+    Conditioner,
+    CosmeticBrush,
+    EnglishMustache,
+    Eyebrow,
+    FacePowder,
+    FacialCleanser,
+    Fingernail,
+    FoundationMakeup,
+    HairBrush,
+    HairClip,
+    HandCream,
+    Handwashing,
+    HandwashingFluid,
+    HealthProducts,
+    Jewelry,
+    LipGloss,
+    Lipstick,
+    LipstickOne,
+    LipTattoo,
+    Lotion,
+    Makeups,
+    Mascara,
+    MassageChair,
+    MassageChairOne,
+    MassageTable,
+    Mirror,
+    MirrorTwo,
+    Mouth,
+    NailPolish,
+    NailPolishOne,
+    Paint,
+    PerfumerBottle,
+    Razor,
+    Reel,
+    Scissors,
+    ShaverOne,
+    Shaving,
+    SoapBubble,
+    SpaCandle,
+    StraightRazor
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkMakeups extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'AfroPick':
+                return <AfroPick size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BarberBrush':
+                return <BarberBrush size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BarberClippers':
+                return <BarberClippers size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BeautyInstrument':
+                return <BeautyInstrument size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                         strokeWidth={strokeWidth}
+                                         strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Blade':
+                return <Blade size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Comb':
+                return <Comb size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Conditioner':
+                return <Conditioner size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'CosmeticBrush':
+                return <CosmeticBrush size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'EnglishMustache':
+                return <EnglishMustache size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Eyebrow':
+                return <Eyebrow size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FacePowder':
+                return <FacePowder size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FacialCleanser':
+                return <FacialCleanser size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Fingernail':
+                return <Fingernail size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FoundationMakeup':
+                return <FoundationMakeup size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                         strokeWidth={strokeWidth}
+                                         strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HairBrush':
+                return <HairBrush size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HairClip':
+                return <HairClip size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HandCream':
+                return <HandCream size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Handwashing':
+                return <Handwashing size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HandwashingFluid':
+                return <HandwashingFluid size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                         strokeWidth={strokeWidth}
+                                         strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HealthProducts':
+                return <HealthProducts size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Jewelry':
+                return <Jewelry size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LipGloss':
+                return <LipGloss size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LipTattoo':
+                return <LipTattoo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Lipstick':
+                return <Lipstick size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LipstickOne':
+                return <LipstickOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Lotion':
+                return <Lotion size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Makeups':
+                return <Makeups size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Mascara':
+                return <Mascara size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MassageChair':
+                return <MassageChair size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MassageChairOne':
+                return <MassageChairOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MassageTable':
+                return <MassageTable size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Mirror':
+                return <Mirror size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MirrorTwo':
+                return <MirrorTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Mouth':
+                return <Mouth size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'NailPolish':
+                return <NailPolish size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'NailPolishOne':
+                return <NailPolishOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Paint':
+                return <Paint size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PerfumerBottle':
+                return <PerfumerBottle size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Razor':
+                return <Razor size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Reel':
+                return <Reel size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Scissors':
+                return <Scissors size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ShaverOne':
+                return <ShaverOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Shaving':
+                return <Shaving size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SoapBubble':
+                return <SoapBubble size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SpaCandle':
+                return <SpaCandle size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'StraightRazor':
+                return <StraightRazor size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkMakeups.defaultProps = {
+    visible: true,
+    name: 'AfroPick',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
