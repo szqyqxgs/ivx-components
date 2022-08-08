e53cf90178cf615f2c657b22226612820a9102af
szqyqxgs <2590708913@qq.com>
2022-08-08 21:18:49 +0800

    新增【美颜调整】图标

---
+++ icon-park/src/operate/components.jsx
@@ -0,0 +1,252 @@
+import React from 'react'
+import {
+    Beauty,
+    Click,
+    ColorFilter,
+    Contrast,
+    ContrastView,
+    ContrastViewCircle,
+    Effects,
+    Erase,
+    FaceRecognition,
+    FigmaMask,
+    Fill,
+    FlipCamera,
+    GameTwo,
+    LatticePattern,
+    LoadingOne,
+    LoadingTwo,
+    Magnet,
+    MaskOne,
+    Material,
+    MaterialTwo,
+    MirrorOne,
+    PicOne,
+    RecycleBin,
+    ReverseRotation,
+    RotatingAdd,
+    RotatingForward,
+    Rotation,
+    ScanSetting,
+    ScreenRotation,
+    Stickers,
+    SwitchTrack
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkOperate extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'Beauty':
+                return <Beauty size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Click':
+                return <Click size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ColorFilter':
+                return <ColorFilter size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Contrast':
+                return <Contrast size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ContrastView':
+                return <ContrastView size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ContrastViewCircle':
+                return <ContrastViewCircle size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Effects':
+                return <Effects size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Erase':
+                return <Erase size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FaceRecognition':
+                return <FaceRecognition size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FigmaMask':
+                return <FigmaMask size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Fill':
+                return <Fill size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'FlipCamera':
+                return <FlipCamera size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'GameTwo':
+                return <GameTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LatticePattern':
+                return <LatticePattern size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LoadingOne':
+                return <LoadingOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LoadingTwo':
+                return <LoadingTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Magnet':
+                return <Magnet size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MaskOne':
+                return <MaskOne size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Material':
+                return <Material size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MaterialTwo':
+                return <MaterialTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'MirrorOne':
+                return <MirrorOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PicOne':
+                return <PicOne size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RecycleBin':
+                return <RecycleBin size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ReverseRotation':
+                return <ReverseRotation size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RotatingAdd':
+                return <RotatingAdd size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RotatingForward':
+                return <RotatingForward size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Rotation':
+                return <Rotation size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ScanSetting':
+                return <ScanSetting size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ScreenRotation':
+                return <ScreenRotation size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Stickers':
+                return <Stickers size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SwitchTrack':
+                return <SwitchTrack size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkOperate.defaultProps = {
+    visible: true,
+    name: 'Beauty',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
