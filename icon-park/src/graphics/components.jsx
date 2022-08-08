8a6d43d813de4fc2b720e1f7395df5da98b9bf7e
szqyqxgs <2590708913@qq.com>
2022-08-08 09:17:51 +0800

    新增【几何图形】图标

---
+++ icon-park/src/graphics/components.jsx
@@ -0,0 +1,195 @@
+import React from 'react'
+import {
+    DiamondOne,
+    DiamondThree,
+    DiamondTwo,
+    HexagonOne,
+    Octagon,
+    OvalOne,
+    Parallelogram,
+    PentagonOne,
+    Quadrilateral,
+    Rectangle,
+    RectangleOne,
+    RectangleSmall,
+    RightAngle,
+    Round,
+    Square,
+    SquareSmall,
+    Trapezoid,
+    Triangle
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkGraphics extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'DiamondOne':
+                return <DiamondOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'DiamondThree':
+                return <DiamondThree size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'DiamondTwo':
+                return <DiamondTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'HexagonOne':
+                return <HexagonOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Octagon':
+                return <Octagon size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'OvalOne':
+                return <OvalOne size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Parallelogram':
+                return <Parallelogram size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PentagonOne':
+                return <PentagonOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Quadrilateral':
+                return <Quadrilateral size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Rectangle':
+                return <Rectangle size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RectangleOne':
+                return <RectangleOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RectangleSmall':
+                return <RectangleSmall size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RightAngle':
+                return <RightAngle size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Round':
+                return <Round size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Square':
+                return <Square size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SquareSmall':
+                return <SquareSmall size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Trapezoid':
+                return <Trapezoid size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Triangle':
+                return <Triangle size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkGraphics.defaultProps = {
+    visible: true,
+    name: 'DiamondOne',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
