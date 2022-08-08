3f487fd104c60f7ae35e1786e71c45030f43732f
szqyqxgs <2590708913@qq.com>
2022-08-08 20:42:05 +0800

    新增【链接】图标

---
+++ icon-park/src/connect/components.jsx
@@ -0,0 +1,357 @@
+import React from 'react'
+import {
+    Anchor,
+    AnchorOne,
+    AnchorRound,
+    AnchorSqure,
+    Api,
+    AppletClosed,
+    AssemblyLine,
+    BezierCurve,
+    Branch,
+    BranchOne,
+    BranchTwo,
+    Clue,
+    Command,
+    Connect,
+    ConnectAddressOne,
+    ConnectAddressTwo,
+    Connection,
+    ConnectionBox,
+    ConnectionPoint,
+    ConnectionPointTwo,
+    Fork,
+    LeftAndRightBranch,
+    LeftBranch,
+    LinkBreak,
+    LinkCloud,
+    LinkCloudFaild,
+    LinkCloudSucess,
+    LinkFour,
+    LinkInterrupt,
+    LinkThree,
+    LowerBranch,
+    NetworkTree,
+    PullRequests,
+    RelationalGraph,
+    RightBranch,
+    RightBranchOne,
+    RightBranchTwo,
+    Ring,
+    RingOne,
+    ShareOne,
+    SplitTurnDownLeft,
+    SplitTurnDownRight,
+    STurnDown,
+    STurnLeft,
+    STurnRight,
+    STurnUp,
+    TreeDiagram,
+    UTurnDown,
+    UTurnLeft,
+    UTurnRight,
+    UTurnUp,
+    WholeSiteAccelerator
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkConnect extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'Anchor':
+                return <Anchor size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AnchorOne':
+                return <AnchorOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AnchorRound':
+                return <AnchorRound size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AnchorSqure':
+                return <AnchorSqure size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Api':
+                return <Api size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                            strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AppletClosed':
+                return <AppletClosed size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AssemblyLine':
+                return <AssemblyLine size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BezierCurve':
+                return <BezierCurve size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Branch':
+                return <Branch size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BranchOne':
+                return <BranchOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'BranchTwo':
+                return <BranchTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Clue':
+                return <Clue size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Command':
+                return <Command size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Connect':
+                return <Connect size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ConnectAddressOne':
+                return <ConnectAddressOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                          strokeWidth={strokeWidth}
+                                          strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ConnectAddressTwo':
+                return <ConnectAddressTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                          strokeWidth={strokeWidth}
+                                          strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Connection':
+                return <Connection size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ConnectionBox':
+                return <ConnectionBox size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ConnectionPoint':
+                return <ConnectionPoint size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ConnectionPointTwo':
+                return <ConnectionPointTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Fork':
+                return <Fork size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LeftAndRightBranch':
+                return <LeftAndRightBranch size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LeftBranch':
+                return <LeftBranch size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkBreak':
+                return <LinkBreak size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkCloud':
+                return <LinkCloud size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkCloudFaild':
+                return <LinkCloudFaild size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkCloudSucess':
+                return <LinkCloudSucess size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkFour':
+                return <LinkFour size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkInterrupt':
+                return <LinkInterrupt size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LinkThree':
+                return <LinkThree size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'LowerBranch':
+                return <LowerBranch size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'NetworkTree':
+                return <NetworkTree size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PullRequests':
+                return <PullRequests size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RelationalGraph':
+                return <RelationalGraph size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RightBranch':
+                return <RightBranch size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RightBranchOne':
+                return <RightBranchOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RightBranchTwo':
+                return <RightBranchTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Ring':
+                return <Ring size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RingOne':
+                return <RingOne size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'STurnDown':
+                return <STurnDown size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'STurnLeft':
+                return <STurnLeft size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'STurnRight':
+                return <STurnRight size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'STurnUp':
+                return <STurnUp size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ShareOne':
+                return <ShareOne size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SplitTurnDownLeft':
+                return <SplitTurnDownLeft size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                          strokeWidth={strokeWidth}
+                                          strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'SplitTurnDownRight':
+                return <SplitTurnDownRight size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'TreeDiagram':
+                return <TreeDiagram size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UTurnDown':
+                return <UTurnDown size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UTurnLeft':
+                return <UTurnLeft size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UTurnRight':
+                return <UTurnRight size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UTurnUp':
+                return <UTurnUp size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'WholeSiteAccelerator':
+                return <WholeSiteAccelerator size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                             strokeWidth={strokeWidth}
+                                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkConnect.defaultProps = {
+    visible: true,
+    name: 'Anchor',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
