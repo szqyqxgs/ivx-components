b7408cfbaa9506f85020aa5c367c22c29e076862
szqyqxgs <2590708913@qq.com>
2022-08-09 21:17:48 +0800

    新增【星座】图标

---
+++ icon-park/src/constellation/components.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import {
+    Aquarius,
+    Aries,
+    Cancer,
+    Capricornus,
+    Gemini,
+    Leo,
+    Libra,
+    Pisces,
+    Sagittarius,
+    Scorpio,
+    Taurus,
+    Virgo
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkConstellation extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'Aquarius':
+                return <Aquarius size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Aries':
+                return <Aries size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Cancer':
+                return <Cancer size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Capricornus':
+                return <Capricornus size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Gemini':
+                return <Gemini size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Leo':
+                return <Leo size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                            strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Libra':
+                return <Libra size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Pisces':
+                return <Pisces size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Sagittarius':
+                return <Sagittarius size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Scorpio':
+                return <Scorpio size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Taurus':
+                return <Taurus size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Virgo':
+                return <Virgo size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkConstellation.defaultProps = {
+    visible: true,
+    name: 'Aquarius',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
