709d86f8c225c0a157c67da0f470fcccd32cd6de
szqyqxgs <2590708913@qq.com>
2022-08-10 13:56:06 +0800

    新增【用户人名】图标

---
+++ icon-park/src/peoples/components.jsx
@@ -0,0 +1,346 @@
+import React from 'react'
+import {
+    AddressBook,
+    AddUser,
+    Appointment,
+    Avatar,
+    Baby,
+    Classroom,
+    EditName,
+    EveryUser,
+    Hands,
+    IdCardH,
+    IdCardV,
+    Me,
+    Passport,
+    People,
+    PeopleBottom,
+    PeopleBottomCard,
+    PeopleDelete,
+    PeopleDeleteOne,
+    PeopleDownload,
+    PeopleLeft,
+    PeopleMinus,
+    PeopleMinusOne,
+    PeoplePlus,
+    PeoplePlusOne,
+    PeopleRight,
+    Peoples,
+    PeopleSafe,
+    PeopleSafeOne,
+    PeopleSearch,
+    PeopleSearchOne,
+    PeopleSpeak,
+    PeoplesTwo,
+    PeopleTop,
+    PeopleTopCard,
+    PeopleUnknown,
+    PeopleUpload,
+    PersonalCollection,
+    PersonalPrivacy,
+    PublicToilet,
+    ReduceUser,
+    RightUser,
+    TurnAround,
+    User,
+    UserBusiness,
+    UserPositioning,
+    UserToUserTransmission,
+    WeixinPeopleNearby,
+    Woman,
+    Women,
+    WrongUser
+} from '@icon-park/react'
+import '@icon-park/react/styles/index.css'
+
+export default class IconParkPeoples extends React.Component {
+
+    // 绑定事件和方法
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.showVisible = this.showVisible.bind(this)
+        this.hideVisible = this.hideVisible.bind(this)
+        this.toggleVisible = this.toggleVisible.bind(this)
+    }
+
+    // 初始化周期
+    componentDidMount() {
+        this.initialize()
+    }
+
+    // 更新周期
+    // componentDidUpdate(prevProps) {}
+
+    // 定义组件事件
+    // 初始化
+    initialize() {
+        this.props.onInitialize && this.props.onInitialize()
+    }
+
+    // 点击
+    click() {
+        this.props.onClick && this.props.onClick()
+    }
+
+    // 定义组件方法
+    // 显示
+    showVisible() {
+        this.props.onShowVisible && this.props.onShowVisible({
+            visible: true
+        })
+    }
+
+    // 隐藏
+    hideVisible() {
+        this.props.onHideVisible && this.props.onHideVisible({
+            visible: false
+        })
+    }
+
+    // 交替显示
+    toggleVisible() {
+        this.props.onToggleVisible && this.props.onToggleVisible({
+            visible: !this.props.visible
+        })
+    }
+
+    // 定义组件函数
+    // 图标风格颜色
+    fill(theme) {
+        let {strokeColor, fillColor, outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor} = this.props
+        let twoToneColor = [strokeColor, fillColor]
+        let multiColor = [outerStrokeColor, outerFillColor, innerStrokeColor, innerFillColor]
+        switch (theme) {
+            case 'outline':
+                return strokeColor ? strokeColor : undefined
+            case 'filled':
+                return fillColor ? fillColor : undefined
+            case 'two-tone':
+                return strokeColor || fillColor ? twoToneColor : undefined
+            case 'multi-color':
+                return outerStrokeColor || outerFillColor || innerStrokeColor || innerFillColor ? multiColor : undefined
+            default:
+                return
+        }
+    }
+
+    // 选择图标
+    icon() {
+        let {name, size, spin, theme, strokeWidth, strokeLinejoin, strokeLinecap} = this.props
+        switch (name) {
+            case 'AddUser':
+                return <AddUser size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'AddressBook':
+                return <AddressBook size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Appointment':
+                return <Appointment size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Avatar':
+                return <Avatar size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Baby':
+                return <Baby size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Classroom':
+                return <Classroom size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'EditName':
+                return <EditName size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'EveryUser':
+                return <EveryUser size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Hands':
+                return <Hands size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'IdCardH':
+                return <IdCardH size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'IdCardV':
+                return <IdCardV size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Me':
+                return <Me size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Passport':
+                return <Passport size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                 strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'People':
+                return <People size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleBottom':
+                return <PeopleBottom size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleBottomCard':
+                return <PeopleBottomCard size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                         strokeWidth={strokeWidth}
+                                         strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleDelete':
+                return <PeopleDelete size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleDeleteOne':
+                return <PeopleDeleteOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleDownload':
+                return <PeopleDownload size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleLeft':
+                return <PeopleLeft size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleMinus':
+                return <PeopleMinus size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleMinusOne':
+                return <PeopleMinusOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                       strokeWidth={strokeWidth}
+                                       strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeoplePlus':
+                return <PeoplePlus size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeoplePlusOne':
+                return <PeoplePlusOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleRight':
+                return <PeopleRight size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleSafe':
+                return <PeopleSafe size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleSafeOne':
+                return <PeopleSafeOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleSearch':
+                return <PeopleSearch size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleSearchOne':
+                return <PeopleSearchOne size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleSpeak':
+                return <PeopleSpeak size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                    strokeWidth={strokeWidth}
+                                    strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleTop':
+                return <PeopleTop size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleTopCard':
+                return <PeopleTopCard size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleUnknown':
+                return <PeopleUnknown size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                      strokeWidth={strokeWidth}
+                                      strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeopleUpload':
+                return <PeopleUpload size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Peoples':
+                return <Peoples size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                                strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PeoplesTwo':
+                return <PeoplesTwo size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PersonalCollection':
+                return <PersonalCollection size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PersonalPrivacy':
+                return <PersonalPrivacy size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'PublicToilet':
+                return <PublicToilet size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'ReduceUser':
+                return <ReduceUser size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'RightUser':
+                return <RightUser size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'TurnAround':
+                return <TurnAround size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                   strokeWidth={strokeWidth}
+                                   strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'User':
+                return <User size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                             strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UserBusiness':
+                return <UserBusiness size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                     strokeWidth={strokeWidth}
+                                     strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UserPositioning':
+                return <UserPositioning size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                        strokeWidth={strokeWidth}
+                                        strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'UserToUserTransmission':
+                return <UserToUserTransmission size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                               strokeWidth={strokeWidth}
+                                               strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'WeixinPeopleNearby':
+                return <WeixinPeopleNearby size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                           strokeWidth={strokeWidth}
+                                           strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Woman':
+                return <Woman size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'Women':
+                return <Women size={size} spin={spin} theme={theme} fill={this.fill(theme)} strokeWidth={strokeWidth}
+                              strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            case 'WrongUser':
+                return <WrongUser size={size} spin={spin} theme={theme} fill={this.fill(theme)}
+                                  strokeWidth={strokeWidth}
+                                  strokeLinejoin={strokeLinejoin} strokeLinecap={strokeLinecap}/>
+            default:
+                return
+        }
+    }
+
+    render() {
+        // 属性、事件、函数
+        let {visible} = this.props
+        return visible ? <span onClick={this.click}>{this.icon()}</span> : null
+    }
+}
+
+// 默认属性
+IconParkPeoples.defaultProps = {
+    visible: true,
+    name: 'AddUser',
+    size: '16',
+    spin: false,
+    theme: 'outline',
+    strokeColor: null,
+    fillColor: null,
+    outerStrokeColor: null,
+    outerFillColor: null,
+    innerStrokeColor: null,
+    innerFillColor: null,
+    strokeWidth: 4,
+    strokeLinecap: null,
+    strokeLinejoin: null
+}
